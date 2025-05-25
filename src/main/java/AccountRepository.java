3024d678ddf7213387e3d518dc29806ac4f9792b
defford <34922037+defford@users.noreply.github.com>
2025-05-25 11:28:16 -0230

    Added repository logic

--- src/main/java/AccountRepository.java
+++ src/main/java/AccountRepository.java
@@ -1,2 +1,19 @@
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 public class AccountRepository {
+    private final HashMap<String, AccountObject> accounts = new HashMap<>();
+
+    public void addAccount(AccountObject account) {
+        accounts.put(account.getId(), account);
+    }
+
+    public AccountObject getAccount(String id) {
+        AccountObject account = accounts.get(id);
+        if (account == null) {
+            throw new NoSuchElementException(id);
+        }
+        return account;
+    }
+
 }
