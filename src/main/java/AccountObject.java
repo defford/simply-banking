e79837b1300e4d0834a4e41cd1a820dbe62e1cb1
defford <34922037+defford@users.noreply.github.com>
2025-05-25 11:01:24 -0230

    Added account object

---
+++ src/main/java/AccountObject.java
@@ -0,0 +1,44 @@
+public class AccountObject {
+    private String id;
+    private String owner;
+    private float balance;
+
+    public AccountObject(String id, String owner, float balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", owner='" + owner + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+}
